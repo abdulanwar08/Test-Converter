0d5e15fe1aa7003a01d17bf50310f769ab84fddd
Abdul Anwar <abdul.k02@tangoe.com>
2021-01-13 12:27:55 +0530

    updated batch file to generate pentaho logs

--- TestConverter/RunTest.bat
+++ TestConverter/RunTest.bat
@@ -2,22 +2,17 @@
 FOR /F "tokens=1,2 delims==" %%G IN (config.properties) DO (set %%G=%%H)  
 
 set LOGFILE=pentaho_log.log
-call :LOG > %LOGFILE%
-exit /B
 
-:LOG
 echo "Starting Job.."
-
 
 if %ExecutableFiles%==2 (
 	echo "Multiple transformation is Initiated"
-	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" "-param:input=%input%"
-	call %base_dir%pan.bat /file:"%file_path2%" "/level=Basic" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path2%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
 	) else (
-	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
 	)
 
 echo "Pentaho Job Completed.."
 call "%PYTHON_HOME%\python.exe" ".\src\TestConverters.py"
-
 
