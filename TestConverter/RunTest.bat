bd850859cf417047334acc9f7c63cd10baa63068
Abdul Anwar <abdul.k02@tangoe.com>
2021-02-08 18:04:12 +0530

    updated batch file

--- TestConverter/RunTest.bat
+++ TestConverter/RunTest.bat
@@ -7,10 +7,10 @@ echo "Starting Job.."
 
 if %ExecutableFiles%==2 (
 	echo "Multiple transformation is Initiated"
-	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
-	call %base_dir%pan.bat /file:"%file_path2%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\\%LOGFILE%" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path2%" "/level=Basic" /logfile="%input%\\%LOGFILE%" "-param:input=%input%"
 	) else (
-	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\%LOGFILE%" "-param:input=%input%"
+	call %base_dir%pan.bat /file:"%file_path1%" "/level=Basic" /logfile="%input%\\%LOGFILE%" "-param:input=%input%"
 	)
 
 echo "Pentaho Job Completed.."
